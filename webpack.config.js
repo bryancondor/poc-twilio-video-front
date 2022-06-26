cd7fe9e1db5776a97b78055edbadccb6fe3a9cc4
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-06-25 21:02:23 -0500

    setup enviroment with webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Client Video Streaming',
+            template: 'index.ejs'
+        }),
+    ]
+};
