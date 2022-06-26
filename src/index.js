3b9f513675289c9b4906ecf84b7632d8f709a184
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-06-25 19:09:36 -0500

    webpack: Loading Data & customize parsers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import _ from 'lodash'
 import './style.css';
 import Icon from './icon.png';
+import Data from './data.xml';
+import Notes from './data.csv';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log('from TOML:', toml.title); // output `TOML Example`
+console.log('from TOML:', toml.owner.name); // output `Tom Preston-Werner`
+
+console.log('from YAML: ', yaml.title); // output `YAML Example`
+console.log('from YAML: ', yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log('from JSON5: ', json.title); // output `JSON5 Example`
+console.log('from JSON5: ', json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
@@ -14,6 +28,9 @@ function component() {
     myIcon.src = Icon;
 
     element.appendChild(myIcon);
+
+    console.log('from XML: ', Data);
+    console.log('from CSV: ', Notes);
 
     return element;
 }
