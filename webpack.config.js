9a876000a9fdf48357b7d153a11cc0fd1eba8fd8
Bryan Alexander Condor De La Cruz <bryanalexandercd@gmail.com>
2022-06-25 20:30:27 -0500

    webpack - Development: Choosing a Development Tool [Using webpack-dev-middleware] (webpack to server, eg. backend node)

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true
+        clean: true,
+        publicPath: '/',
     },
     // was added because in this example we have more than one entrypoint on a single HTML page
     optimization: {
